87ca5c5bbe63ea3c9227fdd4a01ccdc1ce723323
Sam James <sam@gentoo.org>
2023-01-06 05:39:43 +0000

    fix-gnustack: add 'set -x' in tests for easier debugging
    It's hard to see why something failed otherwise, as we only have the
    exit code.
    
    Signed-off-by: Sam James <sam@gentoo.org>

--- misc/fix-gnustack/tests/gnustacktest.sh
+++ misc/fix-gnustack/tests/gnustacktest.sh
@@ -16,6 +16,7 @@
 #    You should have received a copy of the GNU General Public License
 #    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #
+set -x
 
 before=$(../fix-gnustack -f bad-gnustack)
 before=$(echo ${before} | awk '{ print $2 }')
